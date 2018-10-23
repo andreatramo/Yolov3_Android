c4b4dc5ad6cb79aa35d03dc4f8d018440f7d57ad
Andrea Gaetano Tramontano <andreagtramo@gmail.com>
2018-10-23 11:52:36 +0100

    Path update
    Now the app work, the path are correct and the Darknet read correctly the parameter from the .cfg and .weights file. 
    TODO: take frame from camera and process them.

--- app/src/main/java/uk/lcas/andreatramo/yolov3_objectdetector/MainActivity.java
+++ app/src/main/java/uk/lcas/andreatramo/yolov3_objectdetector/MainActivity.java
@@ -1,18 +1,22 @@
 package uk.lcas.andreatramo.yolov3_objectdetector;
 
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import org.opencv.imgcodecs.Imgcodecs;
 
+import org.opencv.core.Mat;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.res.AssetManager;
 
 
-import org.opencv.core.Mat;
-
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,36 +31,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String rootPath = getFilesDir().getPath();
+
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText("Noooooo!!!");
+        tv.setText("GOOOOOOO");
 
-        File file = Environment.getExternalStorageDirectory();
-        String root = file.getAbsolutePath();
-        String path = "./assets/yolo_config/yolov3-tiny.cfg";
-        tv.setText(path);
-
-
-        String cfg_file = "./assets/yolo_config/yolov3-tiny.cfg";
-        String weigth_file = root + "/assets/yolo_config/yolov3-tiny.weigth";
+        String cfg_file = getPath(".cfg", this.getApplicationContext());
+        String weigth_file = getPath(".weights", this.getApplicationContext());
         float conf_thr = 0.5f;
-        String classNames_file = root + "/assets/yolo_config/coco.names";
+        String classNames_file = getPath(".names", this.getApplicationContext());
+        String img_file = getPath(".jpg", this.getApplicationContext());
+
 
         create(cfg_file, weigth_file, conf_thr, classNames_file);
 
-        Mat input_frame = Imgcodecs.imread(root + "/assets/data/aeroplane.jpg"); //the frame to send to classify
-        Imgcodecs.imwrite(root + "/assets/data/aeroplane_2.jpg", input_frame);
+        Mat input_frame = Imgcodecs.imread(img_file); //the frame to send to classify
 
         Mat output_frame = new Mat();
 
-        //classify(input_frame.getNativeObjAddr(), output_frame.getNativeObjAddr());
+        classify(input_frame.getNativeObjAddr(), output_frame.getNativeObjAddr());
 
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
+    @SuppressLint("LongLogTag")
+    private static String getPath(String fileType, Context context) {
+        AssetManager assetManager = context.getAssets();
+        String[] pathNames = {};
+        String fileName = "";
+        System.out.println("-----------------------------------------------------------------------");
+        try {
+            pathNames = assetManager.list("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for ( String filePath : pathNames ) {
+            System.out.println(filePath);
+            if ( filePath.endsWith(fileType)) {
+                fileName = filePath;
+                break;
+            }
+        }
+        BufferedInputStream inputStream;
+        try {
+            // Read data from assets.
+            inputStream = new BufferedInputStream(assetManager.open(fileName));
+            byte[] data = new byte[inputStream.available()];
+            inputStream.read(data);
+            inputStream.close();
+
+            // Create copy file in storage.
+            File outFile = new File(context.getFilesDir(), fileName);
+            FileOutputStream os = new FileOutputStream(outFile);
+            os.write(data);
+            os.close();
+            // Return a path to file which may be read in common way.
+            return outFile.getAbsolutePath();
+        } catch (IOException ex) {
+            //Log.i(TAG, "Failed to upload a file");
+        }
+        return "";
+    }
 
     public native void create(String cfg_file, String weights_file, float conf_thr, String classNames_file);
 
